8ed4f19d7323d3665c0004e3f85bdef8689448a7
guan <guanzejian@shanghai.com.cn>
2018-12-10 14:53:06 +0800

    垃圾桶-环卫工绑定

--- public/static/admin/js/trash_huanwei.js
+++ public/static/admin/js/trash_huanwei.js
@@ -2,38 +2,40 @@ layui.use(['element','layer'], function(){
     var layer   = layui.layer,
         element = layui.element;
 
-     // thead中tr固定
-    var oTop1 = $(".oTable1 thead>tr");
-    var oTop2 = $(".oTable2 thead>tr");
-    var oTop1Top = $(".oTable1 thead>tr").offset().top;
-    var oTop2Top = $(".oTable2 thead>tr").offset().top;
-    var sTop = 0;
-
-    function oScroll(a,b){
-        
-        sTop = $(window).scrollTop();
-        if(sTop > a-40){   //-100  防止抖动
-          b.addClass("Top_0");
-          $(".totop").show();
-        }else if(sTop == 0){
-          b.removeClass("Top_0");
-          $(".totop").hide();
-        }
-      
+    // 绑定信息确认
+    function bindinfo(trash,obj){
+      trash.on('click',function(){
+        var trashNum = $(this).html();
+        obj.html(trashNum);
+      })
     };
+              
+    var trash = $(".trash_num"),bind1 = $(".bind1"),hwer = $(".huanwei"),bind2 = $(".bind2");
+    bindinfo(trash,bind1);
+    bindinfo(hwer,bind2);
 
-    $(window).scroll(function(){
-      oScroll(oTop1Top,oTop1);
-      oScroll(oTop2Top,oTop2);
-    });
- 
-  // $(document).ready(function(){
-  //   var index = layer.open({
-  //     title:"绑定信息确认",
-  //     content:'已选垃圾：<span class="trashnumber">123</span><br/>已选环卫：<span class="huanwei_er">AAAA</span>',
-  //     fixed:true,
-  //     btn:"确认绑定",
+    // 垃圾桶绑定信息------环卫工绑定信息
+    function managerInfo(){
+      var index = layer.open({
+        title: '垃圾桶绑定信息'
+        ,content: 'table框',
+        btn:'关闭窗口',
+        btnAlign:'c',
+        area:['500px','400px'],
+        yes:function(){
+
+          layer.close(index);
+        }
+      })
+    }
+    // 垃圾桶管理
+    $(".trashManager").on('click',function(){
+
+      managerInfo()
+    })
+    // 环卫工管理
+    $(".hwManager").on('click',function(){
       
-  //   })
-  // })
+      managerInfo()
+    })
 })
