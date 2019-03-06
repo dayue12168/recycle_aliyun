fe41e2c8858838191ae5550c87e3f146be902348
dragonstonewl <850806377@qq.com>
2019-03-06 21:47:49 +0800

    1

--- app/v1/controller/Api.php
+++ app/v1/controller/Api.php
@@ -4,6 +4,12 @@
  * User: 安远
  * Date: 2019/2/28
  * Time: 16:57
+ 接口示例{"success":"0","message":"失败原因","sign":"jiuhai", "result":{"dustbintotal":"100",
+ "binlist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}],
+ "captotal":"50","caponline":"48","capoffline":"2",
+ "offlinelist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}],
+ "dust1":"10000","dust7":"70000","overflownum":"2",
+ "overflowlist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}]}}
  */
 namespace app\v1\controller;
 use think\Db;
@@ -11,14 +17,17 @@ class Api
 {
     public function bigScreen()
     {
+    	call bigScreenTemp();
         $res='{"success":"1","message":"接口调用成功","sign":"jiuhai","result":{"dustbintotal":"100","binlist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}],"captotal":"50","caponline":"48","capoffline":"2","offlinelist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}],"dust1":"10000","dust7":"70000","overflownum":"2","overflowlist":[{"longitude":"123.12","latitude":"456.45"},{"longitude":"321.32","latitude":"654.65"}]}}';
         return $res;
     }
 
     public function bigScreenTemp()
     {
-        $sql='select * from jh_dustbin_info';
+    	  
+        $sql='select dust_serial,max(longitude) as longitude,max(latitude) as latitude from jh_dustbin_info where dustbin_state=0';
         $res=Db::query($sql);
-        return json_encode($res);
+        print_r($res);
+        //return json_encode($res);
     }
 }
